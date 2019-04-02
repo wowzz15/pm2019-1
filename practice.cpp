ba092b435da80263d91ae8ce838364abe3d01b78
dyhong-snu <45473855+dyhong-snu@users.noreply.github.com>
2019-04-02 11:40:54 +0900

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/*
+	*  TODO: replace "XXXX-XXXXX" to your student number
+	*/
+	cout << "My student number is XXXX-XXXXX" << endl;
+	return 0
+}
