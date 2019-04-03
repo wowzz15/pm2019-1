992ca519803dc834e58504038ea849d713ac5c48
Yeon jiho <snuackr@snu.ac.kr>
2019-04-03 12:00:10 +0900

    2018-19285 황승진

--- practice.cpp
+++ practice.cpp
@@ -6,6 +6,6 @@ int main() {
 	/*
 	*  TODO: replace "XXXX-XXXXX" to your student number
 	*/
-	cout << "My student number is XXXX-XXXXX" << endl;
+	cout << "My student number is 2018-19285" << endl;
 	return 0
 }
